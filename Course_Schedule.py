f56448fcc392497771ba25c76c4f8f251c63847b
AmrutaKoshe <amruta.koshe@gmail.com>
2024-12-25 17:21:50 -0500

    Kahn Algo added

--- Course_Schedule.py
+++ Course_Schedule.py
@@ -120,4 +120,72 @@ Recursive Call Stack- takes O(V) space
 
 Therefore, total space = O(V+E)
 
+--------------------------------------------------------------------------------------------------------------------------------------\
+METHOD 2: KAHN'S ALGORITHM W. BFS
+
+Refer Kahn_Algo.py for the whole explanation.
+But I'll still walkthrough a cyclic graph example here to see how it returns false.
+
+graph = {
+0: [1,2],
+1: [3],
+2: [],
+3: [4],
+4: [1]
+}
+
+in_degree = [0,2,1,1,1]
+
+queue= [0], top_order = [0]
+For 1 and 2, in-degree reduce by 1
+in_degree = [0,1,0,1,1]
+Push 2 to queue.
+
+queue = [2]
+top_order = [0, 2]
+Now, 2 has no neighbours. 
+So the queue is empty and we exit from the while loop.
+
+The length of top_order is not equal to num of nodes. Hence, there is a cycle.
+
+INTUITION Behind why Kahn Algorithm works for Cycle Detection:- 
+If a cycle is present, there will always be nodes with non-zero in-degrees which will never get added to the queue.
+When there is a 0 in_degree, that means all dependencies of that node have been removed, so no other node depends on this node.
+But when there is a cycle, the nodes are inter-dependent on one another, so none of them can have 0 in-degree.
+
+Hence, the while loop breaks without traversing through all nodes and the length of top_order is not equal to num nodes.
+
 '''
+
+def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+    #Adjacency list representation
+    graph = {i:[] for i in range(numCourses)}
+
+    for course, prereq in prerequisites:
+        graph[prereq].append(course)
+    
+    in_degree = [0]*numCourses
+
+    for u in graph:
+        for v in graph[u]:
+            in_degree[v] += 1
+
+    queue = deque([i for i in range(numCourses) if in_degree[i]==0])
+
+    top_order = []
+
+    while queue:
+        node = queue.popleft()
+        top_order.append(node)
+
+        for neighbour in graph[node]:
+            in_degree[neighbour] -= 1
+            if in_degree[neighbour] == 0:
+                queue.append(neighbour)
+
+    if len(top_order) == numCourses:
+        return True
+
+    else:
+        return False
